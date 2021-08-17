2950223347f1fd0f1bdf9244092b251c761f228f
EDavies2020 <emz201080@yahoo.co.uk>
2021-08-17 18:19:21 +0000

    carousel added to recipes page

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.tooltipped').tooltip();
     $('select').formSelect();
+    $('#carousel-auto').carousel();
+     setInterval(function(){
+        $('#carousel-auto').carousel('next');
+    }, 2000);
   });
 
