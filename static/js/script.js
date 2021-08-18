a1715b23c4f6eadb62af669f54931f49f975de3b
EDavies2020 <emz201080@yahoo.co.uk>
2021-08-18 19:51:59 +0000

    add carousel to homepage

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
   $(".sidenav").sidenav({ edge: "right" });
   $('.tooltipped').tooltip();
@@ -9,15 +8,4 @@ $(document).ready(function () {
       'next');
   }, 3000);
 });
-=======
-$(document).ready(function(){
-    $(".sidenav").sidenav({edge: "right"});
-    $('.tooltipped').tooltip();
-    $('select').formSelect();
-    $('#carousel-auto').carousel();
-     setInterval(function(){
-        $('#carousel-auto').carousel('next');
-    }, 2000);
-  });
->>>>>>> 2950223347f1fd0f1bdf9244092b251c761f228f
 
