82c6ee4ad0cd5b3bef46c7cd5749918301c3b537
EDavies2020 <emz201080@yahoo.co.uk>
2021-09-02 19:18:27 +0000

    404 page edited

--- app.py
+++ app.py
@@ -216,6 +216,6 @@ def not_found(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
 
 # change to false before submission
